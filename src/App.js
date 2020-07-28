9371f5c7511bbdb138658b2c13b76af93df22f41
Ryan Whitmore <ryan@whitmorespace.com>
2020-07-28 08:28:21 -0700

    boilerplate setup for noteful app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <h1>App</h1>;
+  }
 }
 
 export default App;
