f55052afb14b5d0df3b67059d45f43f69a173eae
Ryan Whitmore <ryan@whitmorespace.com>
2020-07-28 09:08:43 -0700

    base component structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/no-unused-state */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
+// import data
+import DATA from './DATA.json';
+
+// import components
+import Header from './components/Header';
+import MainPage from './components/main-page/MainPage';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    DATA,
+  };
+
   render() {
-    return <h1>App</h1>;
+    return (
+      <div className="app">
+        <Header />
+        <Route exact path="/" component={MainPage} />
+      </div>
+    );
   }
 }
 
---
+++ src/components/note-page/NoteMain.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+export default class NoteMain extends Component {
+  render() {
+    return (
+      <div className="sidebar">
+        <p>Main</p>
+      </div>
+    );
+  }
+}
